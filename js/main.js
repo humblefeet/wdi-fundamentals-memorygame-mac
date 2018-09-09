85713c1ffce9c619f1e1635464d76b549ff6e1ec
Matthieu Bourgois <matthieubourgois@Matthieus-MBP.lan>
2018-09-09 12:42:11 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-console.log("up and running");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User has flipped " + cardOne);
-console.log("User has flipped " + cardThree);
+console.log("User has flipped " + cardTwo);
+
+if(cardsInPlay.length === 2 ){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+	alert("You've found a match!");
+}else{
+	alert("Sorry, try again.")
+	}
+};
