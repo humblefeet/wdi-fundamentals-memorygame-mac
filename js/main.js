73028567fcbfc11145c430b7d62de465cad8282d
Matthieu Bourgois <matthieubourgois@MacBook-Pro.domain>
2018-09-09 22:04:30 -0700

    added the game board and some logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 var cards = [
 	{
-		rank: "queen",
-		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
+		rank : "queen",
+		suit : "hearts",
+		cardImage : "images/queen-of-hearts.png"
 	},
 	{
-		rank: "queen",
-		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
+		rank : "queen",
+		suit : "diamonds",
+		cardImage : "images/queen-of-diamonds.png"
 	},
 	{
-		rank: "king",
-		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
+		rank : "king",
+		suit : "hearts",
+		cardImage : "images/king-of-hearts.png"
 	},
 	{
-		rank: "king",
-		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
+		rank : "king",
+		suit : "diamonds",
+		cardImage : "images/king-of-diamonds.png"
 	}
 ];
 
@@ -45,9 +45,17 @@ var flipCard = function(cardId){
 };
 
 
-flipCard(0);
-flipCard(1);
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i ++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-Id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('board-game').appendChild(cardElement);
+	};
+};
 
+createBoard();
 
 
 
